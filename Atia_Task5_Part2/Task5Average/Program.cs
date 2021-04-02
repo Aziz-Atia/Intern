6d3fe9b5f386c9534c6ce12167f221629070a287
Atia <atiabintiaziz@gmail.com>
2021-04-02 12:43:34 +0600

    Task 5 part 2 partial

---
+++ Atia_Task5_Part2/Task5Average/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task5Average
+
+{
+
+    public class Numb
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter Values:");
+            double A = Convert.ToDouble(Console.ReadLine());
+            double B = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine((A + B) / 2);
+        }
+    }
+}
